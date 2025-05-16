9f8cf9c6d2455d28d7ff8476f67d7f33fab16379
Tom Auxerre <tom.auxerre@etudiant.univ-lr.fr>
2025-05-16 11:16:08 +0200

    [ADD] - Connect with microsoft logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,4 @@
-<script setup>
-import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
 
 <template>
   <div>
@@ -25,24 +17,31 @@ const count = ref(0)
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
+    <p>
+      user : {{ user.name }}
+    </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <button-sign-in-microsoft />
+
+
 </template>
+
+<script setup>
+import {computed, ref} from 'vue'
+import ButtonSignInMicrosoft from "./buttons/ButtonSignInMicrosoft.vue";
+import {useStore} from "vuex";
+
+defineProps({
+  msg: String,
+})
+const count = ref(0)
+
+const store = useStore()
+const user = computed (() => store.getters.user)
+</script>
+
+
 
 <style scoped>
 .read-the-docs {
---
+++ src/components/buttons/ButtonSignInMicrosoft.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+      @click="login"
+      class="flex items-center gap-2 text-sm text-blue-600 hover:text-blue-800 font-medium px-3 py-1 rounded transition duration-150"
+  >
+    <span>Connexion Microsoft</span>
+
+  </button>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+import {signInAndGetUser} from "../../lib/microsoftGraph.js";
+
+
+const store = useStore()
+
+const login = async () => {
+  signInAndGetUser().then(res => store.dispatch('updateUser', res.idTokenClaims))
+      .catch(err => console.error(err))
+}
+</script>
